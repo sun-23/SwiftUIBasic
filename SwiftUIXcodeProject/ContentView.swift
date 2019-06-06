6f2073dfee3814d998d5e42328f83af1f0d7abf8
sun-23 <twinsamson@gmail.com>
2019-06-06 18:43:55 +0700

    Initial Commit

---
+++ SwiftUIXcodeProject/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIXcodeProject
+//
+//  Created by sun on 6/6/2562 BE.
+//  Copyright © 2562 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
